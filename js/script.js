1bf21d79d7cb822e7cebceb50c3175dc2296746f
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-20 16:19:54 +0430

    show image when click on small image one

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 vanillaZoom.init('#my-gallery')
+
--- js/vanilla-zoom.js
+++ js/vanilla-zoom.js
@@ -2,8 +2,35 @@
 
     let defineLibrary = () => ({
         init : function (galleryId) {
-            console.log('setup gallery')
+            let container = document.querySelector(galleryId);
+
+            if (! container ) {
+                console.error('Please add the correct element')
+                return;
+            }
+            let firstImage = container.querySelector('.small-preview')
+            let zoomedImage = container.querySelector('.zoomed-image')
+
+            if ( ! zoomedImage ) {
+                console.error('please add a .zoomed-image tag')
+                return;
+            }
+            if ( ! firstImage ) {
+                console.log('please add image with .small-preview class to your gallery')
+                return;
+            }
+
+            zoomedImage.style.backgroundImage = `url(${firstImage.src})`
+            
+            container.addEventListener('click', function (e) {
+                let elem = e.target;
+                if (elem.classList.contains('small-preview')) {
+                    zoomedImage.style.backgroundImage = `url(${elem.src})`
+                }
+            })
+
         }
+
     })
 
     if (typeof(vanillaZoom) == 'undefined') {
